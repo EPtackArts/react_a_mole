e214d8ce29cc4df25852a6ba2a4d80efde81a7d4
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-11-19 10:33:24 -0600

    finished

---
+++ src/components/Mole.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react'
+import moleImg from "../mole.png"
+
+function Mole(props){
+    useEffect(() => {
+        // Feel free to adjust this number as you see fit.
+        // It is your game to tune!
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}}
+            src={moleImg}
+            onClick={props.handleClick} />
+        </div>
+    )
+}
+export default Mole
